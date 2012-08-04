487fba2636fb3820669fc0a03aff89f36ec23c67
jean-philippe serafin <serafinjp@gmail.com>
2012-08-04 12:57:32 +0200

    Adds ResumableUploadView

---
+++ resumable/views.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import settings
+
+from django.views.generic import View
+from django.http import Http404, HttpResponse
+from django.core.exceptions import ImproperlyConfigured
+from django.core.files.storage import FileSystemStorage
+
+from .files import ResumableFile
+
+
+class ResumableUploadView(View):
+    def get(self, *args, **kwargs):
+        """Checks if chunk has allready been sended.
+        """
+        r = ResumableFile(self.storage, self.request.GET)
+        if not r.chunk_exists:
+            raise Http404('chunk not found')
+        return HttpResponse('chunk already exists')
+
+    def post(self, *args, **kwargs):
+        """Saves chunks then checks if the file is complete.
+        """
+        chunk = self.request.FILES.get('file')
+        r = ResumableFile(self.storage, self.request.POST)
+        r.process_chunk(chunk)
+        if r.is_complete:
+            self.process_file(r.filename, r.file)
+            r.delete_chunks()
+        return HttpResponse()
+
+    def process_file(self, filename, file):
+        """Process the complete file.
+        """
+        self.storage.save(filename, file)
+
+    @property
+    def storage(self):
+        chunks_root = getattr(settings, 'CHUNKS_ROOT', None)
+        if chunks_root is None:
+            raise ImproperlyConfigured('You must set a CHUNKS_ROOT setting')
+        return FileSystemStorage(location=chunks_root)
