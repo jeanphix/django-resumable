8c7c084ae7a47347cefd755cbc0719b095c93946
jean-philippe serafin <serafinjp@gmail.com>
2012-08-08 20:48:12 +0200

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='django-resumable',
+    version='0.1b',
+    author=u'jean-philippe serafin',
+    author_email='serafinjp@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/jeanphix/django-resumable',
+    license='MIT licence',
+    description='Django resumable uploads',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    zip_safe=False,
+)
