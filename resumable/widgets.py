9d263967fe3e93c32a91b62e103ab1b4bf71c3bb
jean-philippe serafin <serafinjp@gmail.com>
2012-08-08 20:49:11 +0200

    Improves ResumableFileInput

--- resumable/widgets.py
+++ resumable/widgets.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 from mimetypes import guess_type
 
-from django.forms.widgets import FileInput, CheckboxInput
+from django.forms.widgets import FileInput, HiddenInput
 from django.core.files.storage import FileSystemStorage
 from django.core.files.uploadedfile import UploadedFile
+from django.utils.safestring import mark_safe
 
 
 class ResumableFileInput(FileInput):
-    def path_input_hidden_name(self, name):
+    def __init__(self, *args, **kwargs):
+        super(ResumableFileInput, self).__init__(*args, **kwargs)
+        self.filename = None
+
+    def filename_input_name(self, name):
         """Returns the name of the input[type=hidden] that contains the
         uploaded file path.
         """
         return "%s-path" % name
 
     def value_from_datadict(self, data, files, name):
-        filename = data.get(self.path_input_hidden_name(name))
+        filename = data.get(self.filename_input_name(name))
         storage = self.storage
         if filename is not None and len(filename) > 0 \
                 and storage.exists(filename):
+            self.filename = filename
             file = storage.open(filename)
             return UploadedFile(
                 file=file,
@@ -26,6 +32,13 @@ class ResumableFileInput(FileInput):
                 size = storage.size(filename)
             )
         return files.get(name, None)
+
+    def render(self, name, *args, **kwargs):
+        return mark_safe("%s%s" % (
+            HiddenInput().render(self.filename_input_name(name),
+                self.filename, {}),
+            super(ResumableFileInput, self).render(name, *args, **kwargs)
+        ))
 
     @property
     def storage(self):
